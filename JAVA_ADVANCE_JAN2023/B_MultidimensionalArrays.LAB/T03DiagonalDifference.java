3e64a935e3b8d73944e9517acbddb3ee7abbddf5
T-Trifonov <trifonov.mr@gmail.com>
2023-01-26 16:16:27 +0200

    Update T03DiagonalDifference.java

--- JAVA_ADVANCE_JAN2023/B_MultidimensionalArrays.LAB/T03DiagonalDifference.java
+++ JAVA_ADVANCE_JAN2023/B_MultidimensionalArrays.LAB/T03DiagonalDifference.java
@@ -6,7 +6,39 @@ public class T03DiagonalDifference {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.parseInt(scanner.nextLine());
 
+        int[][] matrix = new int[num][num];
+
+        for (int i = 0; i < num; i++) {
+            String[] rowValues = scanner.nextLine().split("\\s+");
+            for (int j = 0; j < num; j++) {
+                matrix[i][j]=Integer.parseInt(rowValues[j]);
+            }
+        }
+
+        int diaT = 0;
+        int diaB = 0;
+
+        int step=0;
+        while(step <= matrix.length-1){
+
+            diaT+=matrix[step][step];
+
+            step++;
+        }
+
+        step=0;
+        int stepB= matrix.length-1;
+        while(stepB>=0){
+
+            diaB+=matrix[stepB][step];
+
+            step++;
+            stepB--;
+        }
+
+        System.out.println(Math.abs(diaT-diaB));
 
     }
 }
